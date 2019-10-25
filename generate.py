045d999f3fc3a747c33c4455556702777c660273
Nathaniel Potrepka <nspotrepka@gmail.com>
2019-10-25 02:13:51 -0700

    Use main function

--- generate.py
+++ generate.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 from deeptone import core
 
-core.print_tensorflow_version()
+def main():
+    core.print_tensorflow_version()
 
-model = core.DeepTone()
+    model = core.DeepTone()
+
+if __name__ == '__main__':
+    main()
